7be4227e380203daf9ff562e74b0e9467db05f9d
walidChaybi <98161047+walidChaybi@users.noreply.github.com>
2023-11-22 10:49:56 +0100

    stripe

--- src/hooks/useBuyCredits.ts
+++ src/hooks/useBuyCredits.ts
@@ -2,7 +2,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import { env } from "~/env.mjs";
 import { api } from "~/utils/api";
 
-const stripePromise = loadStripe(env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+const stripePromise = loadStripe("pk_test_0tla0wz7G0tirAuOGkkBGg8u");
 
 export function useBuyCredits() {
   const checkout = api.checkout.createCheckout.useMutation();
